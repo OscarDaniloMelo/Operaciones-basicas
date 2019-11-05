b6852e7663754eaaa3451b8199c8d2a6916b7095
Oscar Melo <lilo921030@gmail.com>
2019-11-05 01:17:12 -0500

    primer commit

---
+++ js/programa.js
@@ -0,0 +1,30 @@
+var dato1 = null;
+var dato2 = null;
+var dato3 = null;
+
+function Suma(Suma){
+    dato1 = parseInt(prompt("Proporciona el primer número"));
+    dato2 = parseInt(prompt("Proporciona el segundo número"));
+    dato3 = dato1 + dato2;
+    alert("El resultado es:"+" "+dato3);
+}
+function Resta(Resta){
+    dato1 = parseInt(prompt("Proporciona el primer número"));
+    dato2 = parseInt(prompt("Proporciona el segundo número"));
+    dato3 = dato1 - dato2;
+    alert("El resultado es:"+" "+dato3);
+}
+function Multiplicación(Multiplicación){
+    dato1 = parseInt(prompt("Proporciona el primer número"));
+    dato2 = parseInt(prompt("Proporciona el segundo número"));
+    dato3 = dato1 * dato2;
+    alert("El resultado es:"+" "+dato3);  
+}
+function División(División){
+    dato1 = parseInt(prompt("Proporciona el primer número"));
+    dato2 = parseInt(prompt("Proporciona el segundo número"));
+    dato3 = dato1 / dato2;
+    alert("El resultado es:"+" "+dato3);
+}
+
+
